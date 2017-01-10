0003798a3bfeb8e4d87ea18b8c8ba9dc965c2ae8
hechaohe <550121308@qq.com>
2017-01-10 14:43:56 +0800

    Initial Commit

---
+++ test_MVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_MVC
+//
+//  Created by hc on 2017/1/10.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test_MVC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test_MVC
+//
+//  Created by hc on 2017/1/10.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
